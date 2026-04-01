d6d05be93fbb7cb9db6c364fca1968d619cb7085
Barnabas Balogh <bdbarni@gmail.com>
2026-04-01 22:17:09 +0200

    Add image URL prefix and completion helpers to JellyfinImageHelper

--- core/data/src/main/java/hu/bbara/purefin/core/data/image/JellyfinImageHelper.kt
+++ core/data/src/main/java/hu/bbara/purefin/core/data/image/JellyfinImageHelper.kt
@@ -5,6 +5,7 @@ import org.jellyfin.sdk.model.api.ImageType
 
 class JellyfinImageHelper {
     companion object {
+
         fun toImageUrl(url: String, itemId: UUID, type: ImageType): String {
             if (url.isEmpty()) {
                 return ""
@@ -16,6 +17,26 @@ class JellyfinImageHelper {
                 .append("/Images/")
                 .append(type.serialName)
                 .toString()
+        }
+
+        fun toPrefixImageUrl(url: String, itemId: UUID): String {
+            if (url.isEmpty()) {
+                return ""
+            }
+            return StringBuilder()
+                .append(url)
+                .append("/Items/")
+                .append(itemId)
+                .append("/Images/")
+                .toString()
+        }
+
+        fun finishImageUrl(prefixImageUrl: String, imageType: ImageType): String {
+            return StringBuilder()
+                .append(prefixImageUrl)
+                .append(imageType.serialName)
+                .toString()
+
         }
     }
 }
