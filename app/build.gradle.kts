a41836b925c3c78ca7e8a3daa73d527689e1f1fd
Barnabas Balogh <bdbarni@gmail.com>
2026-05-03 14:04:13 +0200

    chore(build): update versionCode to 1000005 for the upcoming release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "hu.bbara.purefin"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1000002
+        versionCode = 1000005
         versionName = "0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
