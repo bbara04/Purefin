9a83eb635f8209001572e6f9c7ac2b7f4bbb110f
Barnabas Balogh <bdbarni@gmail.com>
2026-05-03 22:12:05 +0200

    chore(build): increment versionCode to 1000008

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "hu.bbara.purefin"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1000006
+        versionCode = 1000008
         versionName = "0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
