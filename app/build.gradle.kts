7a04c59023681a3741c1f635026c9b14b800865c
Barnabas Balogh <bdbarni@gmail.com>
2026-05-07 20:21:57 +0200

    feat: increment version codes for application updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "hu.bbara.purefin"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1000008
+        versionCode = 1000009
         versionName = "0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
