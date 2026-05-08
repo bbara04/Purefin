e05041fd14c196d7dce440e1b4a1c50d0e861429
Barnabas Balogh <bdbarni@gmail.com>
2026-05-08 19:46:46 +0200

    feat: add focus restoration to TabRow in TvSeriesComponents for improved accessibility

--- app-tv/src/main/java/hu/bbara/purefin/ui/screen/series/components/TvSeriesComponents.kt
+++ app-tv/src/main/java/hu/bbara/purefin/ui/screen/series/components/TvSeriesComponents.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.focusRestorer
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
@@ -90,6 +91,7 @@ internal fun TvSeasonTabs(
     TabRow(
         selectedTabIndex = selectedSeasonIndex,
         modifier = modifier
+            .focusRestorer()
             .fillMaxWidth()
             .wrapContentWidth(Alignment.Start),
         contentColor = MaterialTheme.colorScheme.onSurface
