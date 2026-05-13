f5d476b28e857ce61cba5deddf205b518dac7a12
Barnabas Balogh <bdbarni@gmail.com>
2026-05-13 19:14:07 +0200

    feat: increment version code to 2000006

--- app-tv/build.gradle.kts
+++ app-tv/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "hu.bbara.purefin.tv"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2000005
+        versionCode = 2000006
         versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
